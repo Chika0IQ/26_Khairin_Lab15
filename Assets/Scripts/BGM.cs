7b41f20e1b06253690e42c4dda3f125f69a7fa85
Chika0IQ <mohdkhairin62@gmail.com>
2021-01-13 16:19:15 +0800

    Initial commit

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+
+    public AudioClip[] BGMClipArr;  // BGM array
+
+    private AudioSource bgmSource;  // Initaillize AudioSouce
+
+    private float volume;           // Volume variable 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmSource = GetComponent<AudioSource>();  // Get AudioSource Component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioChange();     // Calling AudioChange Function
+        TabKeyPressed();   // Calling TabKeyPressed Function
+    }
+
+    private void TabKeyPressed()
+    {
+
+        // On tab key pressed,  Bgm will change to a random BGM in the array
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            int rand = Random.Range(0, BGMClipArr.Length);
+            bgmSource.PlayOneShot(BGMClipArr[rand]);
+        }
+    }
+
+    private void AudioChange()
+    {
+
+        //Audio will increase or decrease with up or down arrow keys are pressed
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            bgmSource.volume = volume+=0.2f * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            bgmSource.volume = volume-=0.2f * Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public GameObject jumpText;        // Finding the JumpText
+    public AudioClip[] AudioClipArr;   // AudioClip array
+    public Rigidbody playerRb;         // Find RigidBody of Player
+
+    private AudioSource audioSource;   // Initaillize AudioSouce
+
+    private int JumpCounter;           // Count how many times player has jumped
+    public float jump = 10f;           // How hign player can jump
+
+    private int spacePressed = 0;      // Count how many times spacebar is pressed for Single Jump
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();         // Get Rigidbody component
+        audioSource = GetComponent<AudioSource>();    // Get AudioSource Component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerJump();  // Calling PlayerJump Function
+    }
+
+    private void PlayerJump()
+    {
+
+        // Player will jump 1 at a time after landing on platfrom, add JumpCounter to Jumpcounter Text and produce audio on every Jump
+        if (Input.GetKeyDown(KeyCode.Space) && spacePressed < 1)
+        {
+
+            playerRb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            JumpCounter++;
+            spacePressed++;
+            jumpText.GetComponent<Text>().text = "Jump: " + JumpCounter;
+
+            //audioSource.Play();
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if player has landed on gameobject.Tag "Platfrom"
+        if(collision.gameObject.CompareTag("Platform"))
+        {
+            spacePressed = 0;
+        }
+    }
+}
